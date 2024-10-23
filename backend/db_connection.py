ad390f36e9d8ad98fc27336be10288c78727c15f
zhelus <zhelus@rocketmail.com>
2024-10-23 14:56:02 -0500

    initial test successfull

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,8 +1,33 @@
 #connection file
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
 
-uri = DB_CONNECTION_STRING
+def load_env_file(filepath):
+    # Open the .env file and automatically close it after reading
+    with open(filepath) as file:
+        for line in file:
+            # Strip leading/trailing whitespace and skip empty lines or comments
+            line = line.strip()
+            if line and not line.startswith('#'):
+                # Split the line into key and value by the first '='
+                key, value = line.split('=', 1)
+                
+                # Set the key-value pair as an environment variable
+                os.environ[key] = value
+# Load environment variables from the mongodb.env file
+load_env_file('mongodb.env')
+
+# Access the variables from the environment
+db_admin_account = os.getenv('DB_ADMIN_ACCOUNT')
+db_admin_password = os.getenv('DB_ADMIN_PASSWORD')
+db_connection_string = os.getenv('DB_CONNECTION_STRING')
+# end load
+
+
+print(f"Imported Connection String: {db_connection_string}")
+"""
+uri = db_connection_string
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -23,3 +48,4 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+"""
